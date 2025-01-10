31cfabee8ce4e4d587dc55d23d36929892982805
MacBook Pro <macbookpro@Claudias-MacBook-Pro.local>
2025-01-10 18:38:48 -0400

    first commit 4

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(_name_)
+
+# Cargar el modelo y el scaler
+model = joblib.load('models/Bkm-creditRisk.joblib')
+scaler = joblib.load('models/normalizadorScoring.joblib')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json(force=True)
+        # Suponiendo que los datos enviados son una lista de características
+        features = np.array(data['features']).reshape(1, -1)
+        features_scaled = scaler.transform(features)  # Normaliza los datos
+        prediction = model.predict(features_scaled)  # Predicción con el modelo
+        return jsonify({'prediction': prediction.tolist()})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=5000)
